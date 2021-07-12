7e7cddfca740c5f68fb305b1313f639022ad3e5b
rohitnairtech <puthenrohit@gmail.com>
2021-07-12 16:55:16 +0530

    Initial screens built

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+//import 'package:logging/logging.dart';
+
+import 'screens/homePage.dart';
+
+import 'screens/loginPage.dart';
+
+void main() {
+  checkLogin();
+  runApp(MyApp());
+}
+
+bool loggedIn = false;
+
+Future<http.Response> checkLogin() async {
+  final response =
+      await http.get(Uri.parse('https://dashboard.aibuddha.in/api/checklogin'));
+
+  print(response.statusCode);
+  if (response.statusCode == 200) {
+    loggedIn = true;
+  } else {
+    loggedIn = false;
+  }
+  return response;
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aibuddha - Admin App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: (loggedIn ? HomePage() : LoginPage()),
+    );
+  }
+}
